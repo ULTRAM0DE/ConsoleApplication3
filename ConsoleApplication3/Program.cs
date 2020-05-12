51297aa738e4b012b23ea9101a2022c309a5d52e
0NE <0netw08778@gmail.com>
2020-05-12 16:58:30 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { };
+            while (true)
+            {
+                Console.WriteLine("Введите  имя  ");
+                string name = Console.ReadLine();
+                names.Add(name);
+                Console.WriteLine("Нажмите  1 что  бы  продолжить 2 что бы  выйти ");
+
+                if (Console.ReadLine() == "2")
+                { break; }
+
+            }
+            names.Sort();
+            names = names.Distinct().ToList();
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            
+
+            }
+            Console.WriteLine("Всего имен" + names.Count);
+            Console.ReadLine();
+        }
+    }
+}
